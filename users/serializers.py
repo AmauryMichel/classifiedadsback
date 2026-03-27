00eccafcb8df170517fa006748c84709c80e767b
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-03-27 11:03:26 +0100

    feat(user): added password hashing

--- users/serializers.py
+++ users/serializers.py
@@ -15,3 +15,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ["url", "id", "email", "password", "first_name", "last_name"]
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
