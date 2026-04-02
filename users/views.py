91e5567290ad93aa95e64417e9afec96852c9de2
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-04-02 20:00:17 +0200

    feat(auth): added auth requirement for user routes

--- users/views.py
+++ users/views.py
@@ -4,8 +4,20 @@ from rest_framework import permissions, viewsets
 from users.models import User
 from users.serializers import UserSerializer
 
+class UserPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        
+        if view.action == 'create':
+            return True
+        elif view.action in ['list', 'update', 'partial_update', 'destroy']:
+            return request.user.is_authenticated
+        
+        return False
+
 # API endpoint that allows users to be viewed or edited.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = []
+    permission_classes = [UserPermission]
