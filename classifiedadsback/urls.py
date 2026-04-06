9dd2b84f83f451489a483d68e54771e9b3c873f4
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-04-06 16:21:49 +0200

    feat(auth): added login route

--- classifiedadsback/urls.py
+++ classifiedadsback/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users import views
 
@@ -29,6 +33,9 @@ register = views.UserViewSet.as_view({"post": "create"})
 urlpatterns = [
     path("", include(router.urls)),
     path("auth/register/", register, name="register"),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),  
 ]
