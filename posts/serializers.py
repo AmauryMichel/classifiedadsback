1f3761c7448d11cdb2bc8ff8630a7446ccb59a59
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-04-28 09:29:42 +0200

    feat(post): added read only field with creator data

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,6 +4,8 @@ from django.contrib.auth.hashers import make_password
 from posts.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    creator_data = UserSerializer(read_only=True, source='creator')
+
     class Meta:
         model = Post
-        fields = ["url", "id", "creator", "title", "text", "active", "creation_date", "categories"]
+        fields = ["url", "id", "creator", "creator_data", "title", "text", "active", "creation_date", "categories"]
