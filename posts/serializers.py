7da0b70e077088d96c54f21d6b0eaef94d5cc196
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-05-01 19:01:42 +0200

    feat(category): added serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 
-from posts.models import Post
+from users.serializers import UserSerializer
+from posts.models import Post, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["url", "id", "name", "description"]
 
 class PostSerializer(serializers.ModelSerializer):
     creator_data = UserSerializer(read_only=True, source='creator')
+    categories_info = CategorySerializer(read_only=True, source='categories', many=True)
 
     class Meta:
         model = Post
-        fields = ["url", "id", "creator", "creator_data", "title", "text", "active", "creation_date", "categories"]
+        fields = ["url", "id", "creator", "creator_data", "title", "text", "active", "creation_date", "categories", "categories_info"]
+        extra_kwargs = {
+            'creator': {'write_only': True},
+        }
